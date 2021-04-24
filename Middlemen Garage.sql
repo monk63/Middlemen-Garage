64ebd292a75b57e343bfadcb9d9c0958e555ad25
Mike Ofori <61565391+monk63@users.noreply.github.com>
2021-04-24 15:05:49 +0000

    Update Middlemen Garage.sql

--- Middlemen Garage.sql
+++ Middlemen Garage.sql
@@ -1,12 +1,12 @@
-
-
+-- Michael Nana Kofi Ofori
+--  MIDDLEMEN_GARAGE_74212023;
+  
+ -- Create a database called ‘MIDDLEMEN_GARAGE’  
 DROP DATABASE IF EXISTS MIDDLEMEN_GARAGE_74212023;
 CREATE DATABASE  MIDDLEMEN_GARAGE_74212023;
 USE MIDDLEMEN_GARAGE_74212023;
 
-
-
-
+-- User Table
 CREATE TABLE  User (
   user_id int auto_increment primary key,
   fname varchar(60)  not null,
@@ -18,10 +18,9 @@ CREATE TABLE  User (
  address varchar(50) not null,
  email varchar(40) unique CHECK (email LIKE '%@%') NOT NULL,
  state varchar(50) not null
- 
---  PRIMARY KEY (user_id)
 );
 
+-- Vehicle Table
 CREATE TABLE  Vehicle (
   vehicle_vin int auto_increment primary key,
   vehicle_make varchar(60) not null,
@@ -34,7 +33,6 @@ CREATE TABLE  Vehicle (
  condition_ varchar(30),
  price DECIMAL(6,2)
   
---  PRIMARY KEY (vehicle_vin)
 );
 
 -- Customer Table
@@ -43,24 +41,23 @@ CREATE TABLE  Customer (
   user_id int not null,
   vehicle_vin int not null,
   
- -- PRIMARY KEY (customer_id),
-  foreign key (user_id) references User(user_id),
-  foreign key (vehicle_vin) references Vehicle(vehicle_vin)
+  foreign key (user_id) references User(user_id)    ON DELETE CASCADE,
+  foreign key (vehicle_vin) references Vehicle(vehicle_vin)   ON DELETE CASCADE
 );
 
-
+-- SalesPerson table
 CREATE TABLE SalesPerson (
   salesperson_id int auto_increment primary key,
   user_id int,
   vehicle_vin int,
   customer_id int not null,
   
- -- PRIMARY KEY (salesperson_id),
-  foreign key (user_id) references User(user_id),
-  foreign key (vehicle_vin) references Vehicle(Vehicle_vin),
-	foreign key (customer_id) references Customer(customer_id)
+  foreign key (user_id) references User(user_id)  ON DELETE CASCADE,
+  foreign key (vehicle_vin) references Vehicle(Vehicle_vin)  ON DELETE CASCADE,
+	foreign key (customer_id) references Customer(customer_id)  ON DELETE CASCADE
 );
 
+-- Garage Table
 CREATE TABLE Garage (
   garage_id int auto_increment primary key,
   garage_name  varchar(60) not null,
@@ -71,25 +68,25 @@ CREATE TABLE Garage (
   contact varchar(20) not null unique,
   salesperson_id int not null,
   customer_id int not null,
- 
- -- PRIMARY KEY (garage_id),
- foreign key (Salesperson_id) references Salesperson(Salesperson_id),
- foreign key (customer_id) references Customer(customer_id)
+
+ foreign key (Salesperson_id) references Salesperson(Salesperson_id) ON DELETE CASCADE,
+ foreign key (customer_id) references Customer(customer_id) ON DELETE CASCADE
 );
 
+-- Supplier Table
 CREATE TABLE Supplier (
   supplier_id int auto_increment primary key,
-  supplier_name  varchar(60),
+  supplier_name  varchar(60) unique,
   vehicle_make varchar(100)
   
- -- PRIMARY KEY (suppliers_id)
 );
 
+-- VehicleForSlae table
 CREATE TABLE  VehicleForSale (
 vehicle_vin int not null,
 price DECIMAL(6,2),
 
-foreign key (vehicle_vin) references Vehicle(vehicle_vin)
+foreign key (vehicle_vin) references Vehicle(vehicle_vin) ON DELETE CASCADE
 );
 
 CREATE TABLE  Appointment (
@@ -97,19 +94,20 @@ CREATE TABLE  Appointment (
     customer_id int not null,
     appointment DATETIME not null,
     
-      foreign key (Salesperson_id) references Salesperson(Salesperson_id),
-	foreign key (customer_id) references Customer(Customer_id)
+      foreign key (Salesperson_id) references Salesperson(Salesperson_id) ON DELETE CASCADE,
+	foreign key (customer_id) references Customer(Customer_id) ON DELETE CASCADE
 );
 
+-- Workshop Table
 CREATE TABLE  Workshop (
   workshop_id int auto_increment primary key,
   garage_id int not null,
   number_of_cars_avaliable int,  
   
-  -- PRIMARY KEY (workshop_id),
-foreign key (garage_id) references Garage(garage_id)
+foreign key (garage_id) references Garage(garage_id) ON DELETE CASCADE
 );
 
+-- Warehouse Table
 CREATE TABLE Warehouse (
    workhouse_name  varchar(60),
    location varchar(60),
@@ -117,19 +115,21 @@ CREATE TABLE Warehouse (
    number_of_cars int,
    workshop_id int not null,
    
-    foreign key (workshop_id) references Workshop(workshop_id)   
+    foreign key (workshop_id) references Workshop(workshop_id)   ON DELETE CASCADE
 );
 
+-- Vehicle Sold table
 CREATE TABLE VehicleSold (
  vehicle_vin int,
  price DECIMAL(6,2),
  date_Sold DATE,
  customer_id int not null,
  
- foreign key (vehicle_vin) references Vehicle(vehicle_vin),
- foreign key (customer_id) references Customer(customer_id)
+ foreign key (vehicle_vin) references Vehicle(vehicle_vin) ON DELETE CASCADE,
+ foreign key (customer_id) references Customer(customer_id) ON DELETE CASCADE
 );
 
+-- Autopart table
 CREATE TABLE  Autopart  (
    workshop_id int not null,
    supplier_id int not null,
@@ -138,16 +138,17 @@ CREATE TABLE  Autopart  (
    price DECIMAL(6,2),
    vehicle_make varchar(100),   
    
-    foreign key (workshop_id) references Workshop(workshop_id) ,  
-    foreign key (supplier_id) references Supplier(supplier_id)   
+    foreign key (workshop_id) references Workshop(workshop_id)  ON DELETE CASCADE,  
+    foreign key (supplier_id) references Supplier(supplier_id)  ON DELETE CASCADE 
 );
 
+-- Car Features Table
 CREATE TABLE Car_Features (
     vehicle_vin int,
-    feature varchar (100),
+    feature varchar (100) unique,
     color varchar(40),
     
-    foreign key (vehicle_vin) references Vehicle(vehicle_vin)
+    foreign key (vehicle_vin) references Vehicle(vehicle_vin) ON DELETE CASCADE
 );
 
 
